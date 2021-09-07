edead50beaba1a88cdd4054fbf771c81617c5f33
William Allen <willallen@compute.cs.tamu.edu>
2021-09-06 23:54:11 -0500

    gitignore

--- 01-AmazonTruck/build/CMakeFiles/amazontruck.dir/DependInfo.cmake
+++ 01-AmazonTruck/build/CMakeFiles/amazontruck.dir/DependInfo.cmake
@@ -1,23 +1,23 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Users/WilliamAllen/Desktop/School/Fall2021School/221/01-AmazonTruck/src/AmazonTruck.cpp" "C:/Users/WilliamAllen/Desktop/School/Fall2021School/221/01-AmazonTruck/build/CMakeFiles/amazontruck.dir/src/AmazonTruck.cpp.obj"
-  "C:/Users/WilliamAllen/Desktop/School/Fall2021School/221/01-AmazonTruck/src/Driver.cpp" "C:/Users/WilliamAllen/Desktop/School/Fall2021School/221/01-AmazonTruck/build/CMakeFiles/amazontruck.dir/src/Driver.cpp.obj"
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ugrads/w/willallen/221/01-AmazonTruck/src/AmazonTruck.cpp" "/home/ugrads/w/willallen/221/01-AmazonTruck/build/CMakeFiles/amazontruck.dir/src/AmazonTruck.cpp.o"
+  "/home/ugrads/w/willallen/221/01-AmazonTruck/src/Driver.cpp" "/home/ugrads/w/willallen/221/01-AmazonTruck/build/CMakeFiles/amazontruck.dir/src/Driver.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
 
 # The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "../include"
   "../src"
   )
-
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  )
-
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- 01-AmazonTruck/build/CMakeFiles/amazontruck.dir/cmake_clean.cmake
+++ 01-AmazonTruck/build/CMakeFiles/amazontruck.dir/cmake_clean.cmake
@@ -1,13 +1,11 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/amazontruck.dir/src/AmazonTruck.cpp.obj"
-  "CMakeFiles/amazontruck.dir/src/Driver.cpp.obj"
-  "amazontruck.exe"
-  "amazontruck.exe.manifest"
+FILE(REMOVE_RECURSE
+  "CMakeFiles/amazontruck.dir/src/Driver.cpp.o"
+  "CMakeFiles/amazontruck.dir/src/AmazonTruck.cpp.o"
   "amazontruck.pdb"
-  "libamazontruck.dll.a"
+  "amazontruck"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/amazontruck.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/amazontruck.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)

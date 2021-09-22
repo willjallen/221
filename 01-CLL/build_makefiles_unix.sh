9e157f1e6b30e70bb9e8540905b75fa49e139474
WilliamAllen <wallen2268@gmail.com>
2021-09-22 17:39:25 -0500

    Cleanup

--- 01-CLL/build_makefiles_unix.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-dir=$(cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd -P)
-rm -r build
-mkdir build && cd build
-cmake ..
-make
--- 01-CLL/build_makefiles_windows.bat
+++
@@ -1,6 +0,0 @@
-setlocal cd /d %~dp0
-RD /S /Q "build"
-MKDIR build
-CD build
-cmake -G "MinGW Makefiles" ..
-make
